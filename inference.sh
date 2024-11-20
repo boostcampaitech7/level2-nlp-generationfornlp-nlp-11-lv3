efded344611a81237d3d3641496ac1b46df19a59
elwhyjay <lyj4032@naver.com>
2024-11-20 10:04:30 +0000

    chore: 띄어쓰기 오류 수정

--- inference.sh
+++ inference.sh
@@ -1,3 +1,3 @@
 python source/inference.py model=LGAI-EXAONE/EXAONE-3.0-7.8B-Instruct \
-    inference.from_fine_tuning = False \
-    inference.inference_mode = logit
+    inference.from_fine_tuning=False \
+    inference.inference_mode=logit
