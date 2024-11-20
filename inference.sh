7c4b7de2427763fd14891e918325059d75d20372
elwhyjay <lyj4032@naver.com>
2024-11-20 09:57:23 +0000

    fix: permission error 수정

--- inference.sh
+++ inference.sh
@@ -1,3 +1,3 @@
-python inference.py model=LGAI-EXAONE/EXAONE-3.0-7.8B-Instruct \
+python source/inference.py model=LGAI-EXAONE/EXAONE-3.0-7.8B-Instruct \
     inference.from_fine_tuning = False \
     inference.inference_mode = logit
